6a1ce794c544ab4ad6a48eacf14ac43d966f2abd
Aditya Singh <aadityaraj337@gmail.com>
2026-05-28 06:14:39 +0000

    used bits/stdc++.h to include all lib at once

---
+++ Striver-DSA/1-Learn-the-Basics /1.1-Things-to-Know-about-programming /include_all_lib_at_once.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Fast I/O aur input/output files connection
+    #ifndef ONLINE_JUDGE
+    freopen("../../input.txt", "r", stdin);
+    freopen("../../output.txt", "w", stdout);
+    #endif
+
+    int n;
+    cin>>n;
+
+    cout<<"entered number was "<<n<<"\n";
+    
+
+    return 0;
+}
