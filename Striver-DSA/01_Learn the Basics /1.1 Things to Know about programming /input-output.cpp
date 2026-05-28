b9f16e1a41c4c3e7dc28fc1fecafba12e8cc9b2c
Aditya Singh <aadityaraj337@gmail.com>
2026-05-28 05:43:52 +0000

    input output

---
+++ Striver-DSA/01_Learn the Basics /1.1 Things to Know about programming /input-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Fast I/O aur input/output files connection
+    #ifndef ONLINE_JUDGE
+    freopen("../../input.txt", "r", stdin);
+    freopen("../../output.txt", "w", stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    cout<<n<<"\n";
+
+    return 0;
+}
