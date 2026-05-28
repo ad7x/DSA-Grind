51f4349c63dae966df6da6243f28da2254ec3d5f
Aditya Singh <aadityaraj337@gmail.com>
2026-05-28 05:05:13 +0000

    step up codespace

---
+++ Striver-DSA/demo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Ye line hamare code ko input.txt aur output.txt se jodti hai
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int number;
+    cin >> number; // input.txt se number read karega
+    cout << "Aapka input number he: " << number << endl; // output.txt me likhega
+    
+    return 0;
+}
